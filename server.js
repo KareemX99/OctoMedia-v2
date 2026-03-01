e5452b5c9c8575791e7e5e6a3b5b22dbefab3167
Mohamed Dodge <developerdodge@gmail.com>
2026-03-01 18:26:29 +0200

    101

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const schedule = require('node-schedule');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 
 // Security Packages
 const helmet = require('helmet');
